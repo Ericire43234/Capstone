7807d1adef4cd632b92ea009028b42f64c25770f
Brennan Bahr <brennan.m.bahr@gmail.com>
2025-11-18 08:47:48 -0700

    Added iteration to spring and rectangle

--- Capstone/Bar_stress_rectangle.py
+++ Capstone/Bar_stress_rectangle.py
@@ -1,11 +1,14 @@
 #%%Imprts
 import numpy as np
 from CM_Functions import *
+import pandas as pd
 
-def calculation():
+def calculation(E = 0.2e6, Sy=60e3):
 #%% Inputs
-    E=.2*10**6        #Module of Elasticity in lb/in^2
-    Sy=60*10**3        #Yield Strength in lb/in^2
+
+    # Default Values
+    # E=.2*10**6        #Module of Elasticity in lb/in^2
+    # Sy=60*10**3       #Yield Strength in lb/in^2
     l=20             #Length of the beam in in
     w=1.25           #Width of the beam in in
     h=1/32            #Height of the beam in in
@@ -29,11 +32,33 @@ def calculation():
     stress_top=-6*(P*a+n*P*b)/(w*h**2)-n*P/A
     stress_bottom=6*(P*a+n*P*b)/(w*h**2)-n*P/A
     max_stress=max(stress_top, stress_bottom)
-    print("Required Force F: ", F, "lb")
+    # print("Required Force F: ", F, "lb")
     safety_factor=Sy/max_stress
-    print("Safety Factor: ", safety_factor)
+    # print("Safety Factor: ", safety_factor)
+
+    return safety_factor, F
 
 
 
 if __name__ == "__main__":
-    calculation()
+
+    df = pd.read_excel("BookMaterials.xlsx", skiprows=1)
+    materials = df.iloc[:, 0].to_numpy()      # names
+    Epsi  = df.iloc[:, 1].to_numpy()          # property column 1
+    EPa   = df.iloc[:, 2].to_numpy()
+    Sypsi = df.iloc[:, 3].to_numpy()
+    SyPa  = df.iloc[:, 4].to_numpy()
+
+    material_dict = {
+    name: [p1, p2, p3, p4]
+    for name, p1, p2, p3, p4 in zip(materials, Epsi, EPa, Sypsi, SyPa)
+    }   
+
+    #print(material_dict)
+
+    for key in material_dict:
+        value = material_dict[key]
+        n, F = calculation(value[0],value[2])
+        print(f'{key}: n={n:.2f}, F={F:.2f}lbs')
+
+    # calculation()
--- Capstone/PRBM_Spring.py
+++ Capstone/PRBM_Spring.py
@@ -2,11 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt # type: ignore
 from CM_Functions import *
+import pandas as pd
 
-def calculation():
+def calculation(E = 0.2e6, Sy=4e3):
     # Inputs
-    E = 0.2e6            # Modulus of Elasticity in lb/in^2
-    Sy = 4e3             # Yield Strength in lb/in^2
+
+    # Default Inputs
+    # E = 0.2e6            # Modulus of Elasticity in lb/in^2
+    # Sy = 4e3             # Yield Strength in lb/in^2
     l = mm2in(14/2)      # Length of the beam in in
     w = mm2in(6.5)       # Width of the beam in in
     h = mm2in(1)         # Height of the beam in in
@@ -34,18 +37,38 @@ def calculation():
     theta_not=c*theta
     stress_max = P*a*c/(2*I)
     n = Sy/stress_max
-    print("Required Force F: ", F, "lb")
-    print("Maximum Stress: ", stress_max, "lb/in^2")
-    if abs(stress_max)>=Sy:
-        print("Stress exceeds Yield Strength!")
-        print(f'Factor of Safety: {n}')
-    else:
-        print("Stress is within Yield Strength.")
-        print(f'Factor of Safety: {n}')
+    # print("Required Force F: ", F, "lb")
+    # print("Maximum Stress: ", stress_max, "lb/in^2")
+    # if abs(stress_max)>=Sy:
+    #     print("Stress exceeds Yield Strength!")
+    #     print(f'Factor of Safety: {n}')
+    # else:
+    #     print("Stress is within Yield Strength.")
+    #     print(f'Factor of Safety: {n}')
+
+    return n, F
 
 
 
 if __name__ == "__main__":
 
-    mats = {}
-    calculation()
+    df = pd.read_excel("BookMaterials.xlsx", skiprows=1)
+    materials = df.iloc[:, 0].to_numpy()      # names
+    Epsi  = df.iloc[:, 1].to_numpy()          # property column 1
+    EPa   = df.iloc[:, 2].to_numpy()
+    Sypsi = df.iloc[:, 3].to_numpy()
+    SyPa  = df.iloc[:, 4].to_numpy()
+
+    material_dict = {
+    name: [p1, p2, p3, p4]
+    for name, p1, p2, p3, p4 in zip(materials, Epsi, EPa, Sypsi, SyPa)
+    }   
+
+    #print(material_dict)
+
+    for key in material_dict:
+        value = material_dict[key]
+        n, F = calculation(value[0],value[2])
+        print(f'{key}: n={n:.2f}, F={F:.2f}lbs')
+
+    #calculation()
